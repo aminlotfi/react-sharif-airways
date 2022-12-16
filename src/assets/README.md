6935b912d5a1ae3d3492c9eacf33a12799222c11
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2022-12-16 22:03:03 +0330

    fix: folders structure & custom font

---
+++ src/assets/README.md
@@ -0,0 +1,1 @@
+### Global static assets such as images, fonts, svgs, company logo, etc.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class',
   theme: {
     extend: {},
+  },
+  fontFamily: {
+    sans: ['iranyekan', ...defaultTheme.fontFamily.sans],
   },
   plugins: [],
 }
